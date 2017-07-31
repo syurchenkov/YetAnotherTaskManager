6b8fd29a3fa717bc7997af6b2e1bb6ba84116d86
Slava Yurchenkov <yurchenkovslava@gmail.com>
2017-07-31 16:38:44 +0300

    Finish user signup

---
+++ spec/requests/users_signup_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "UsersSignup", type: :request do
+  describe "with invalid infomation" do
+    it "does not change users count" do
+      get signup_path
+      invalid_information = { 
+        user: { 
+          email: "user@invalid",
+          password:              "foo",
+          password_confirmation: "bar" 
+        } 
+      }
+      expect{ post users_path, params: invalid_information }.to_not change { User.count }
+      render_template('users/new')
+    end
+  end
+
+  describe "with valid infomation" do
+    it "change users count by 1" do
+      get signup_path
+      valid_information = {
+        user: {
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+      expect{ post users_path, params: valid_information }.to change { User.count }.by(1)
+      follow_redirect!
+      render_template('users/show')
+    end
+  end
+end
